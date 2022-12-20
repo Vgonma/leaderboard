3539fff8f449847b00062a810d611dbe0cc13ce9
Vgonma <victorgmassimi.microverse@gmail.com>
2022-12-20 09:24:51 -0600

    fix typo for celan to clean

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    celan: true,
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
